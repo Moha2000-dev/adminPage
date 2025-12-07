df7b5ddfca19357ae71b79c7dbd2c89b1890f9bd
muz2020 <mohaalialalawi20@gmail.com>
2025-12-07 09:27:43 +0400

    creat new project

---
+++ src/app/Copmonets/shared/navebar/navebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navebar',
+  imports: [],
+  templateUrl: './navebar.html',
+  styleUrl: './navebar.css',
+})
+export class Navebar {
+
+}
