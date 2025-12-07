ff6ac8e382eb55b76cb886fd77fdb44ace66de5b
muz2020 <mohaalialalawi20@gmail.com>
2025-12-07 12:28:17 +0400

    add the auth system

--- src/app/Copmonets/auth/login/login.ts
+++ src/app/Copmonets/auth/login/login.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from '../../../services/auth';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./Login.css']
 })
-export class Login {
+export class Login implements OnInit {
+  usernames: string = '';
+  passwords: string = '';
+  isloading: boolean = false;
+  errormsg: string = '';
 
+  constructor(private auth: Auth, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  submit(): void {
+    this.isloading = true;
+    const data = { username: this.usernames, password: this.passwords };
+    this.auth.login(data).subscribe(
+      (res) => {
+        console.log(res);
+        this.isloading = false;
+        this.router.navigate(['/user']);
+      },
+        (err) => {
+        console.log(err);
+        this.isloading = false;
+        this.errormsg = 'Invalid username or password';
+      }
+    );
+  }
 }
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
+   private base = "https://fakestoreapi.com/auth/login";
+  constructor( private http: HttpClient) { }
+  login(data: {username:string,password:string}){
+    return this.http.post(this.base,data).pipe(
+      map((data) =>data),catchError((error) => {
+        throw error;
+      })
+    );
+  }
   
 }
