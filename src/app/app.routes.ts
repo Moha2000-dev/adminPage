dcb589e0adeae3fe421959ea98ae093d94dd67cb
muz2020 <mohaalialalawi20@gmail.com>
2025-12-07 10:26:41 +0400

    implemnyte the router methodes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Login } from './Copmonets/auth/login/login';
+import { User } from './Copmonets/user/user';
+import { Products } from './Copmonets/products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full' }, // default route
+
+    {path : 'login', component: Login }, // login route
+
+    {path: 'user',component:User  }, // user route
+    
+    {path: 'products',component:Products} // products route
+
+
+
+
+
+
+];
--- src/app/shared/navebar/navebar.ts
+++ src/app/shared/navebar/navebar.ts
@@ -1,11 +1,19 @@
+import { routes } from './../../app.routes';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navebar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navebar.html',
   styleUrl: './navebar.css',
 })
 export class Navebar {
-
+  //constructor() {} for the logout method
+  constructor(public route: Router) {}
+  //logout method
+  logout() {
+    // Implement logout logic here
+    this.route.navigate(['/login']);
+  }
 }
