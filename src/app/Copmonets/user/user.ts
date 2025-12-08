59f33473813d6f071dc6a2402d91538173d4129c
muz2020 <mohaalialalawi20@gmail.com>
2025-12-08 09:54:29 +0400

    add the git fuctions for the user

--- src/app/Copmonets/user/user.ts
+++ src/app/Copmonets/user/user.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { User as UserService } from './../../services/user';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
+  users = signal<any[]>([]);
+  isloading = signal<boolean>(false);//loading state
+userName: any;
+userEmail: any;
+  constructor(private userService: UserService) {}
 
-}
+  //lodauser
+  loadUsers() {
+    this.isloading.set(true);
+    this.userService.getUsers().subscribe(
+      (res: any) => {
+        this.users.set(res);
+        this.isloading.set(false);
+      },
+      (error) => {
+        console.error('Error fetching users:', error);
+        this.isloading.set(false);
+      }
+    );    
+
+}}
--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,8 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
+  private base = "https://fakestoreapi.com/users";
+  constructor(private http : HttpClient ) { }
+
+  getUsers() {
+    return this.http.get(this.base).pipe(
+      map((data) => data),
+      catchError((error) => {
+        throw 'Error in source. Details: ' + error;
+      })
+    );
+  }
   
 }
