ded2cf17644052f3ea88ad04524ef6901aa3af0f
muz2020 <mohaalialalawi20@gmail.com>
2025-12-11 10:20:17 +0400

    translaters pack

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule } from '@ngx-translate/core';
+
+export function HttploderFactory(http:HttpClient) {
+  return new  TranslateHttpLoader(http, './public/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+
+    ...(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateHttpLoader,
+        useFactory: HttploderFactory,
+        deps: [HttpClient]
+      }
+    }).providers!),
+
   ]
 };
